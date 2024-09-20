3773ad71e20dd9db74b8da5d3749ff6ebab999c1
albemala <albemala@gmail.com>
2024-09-20 21:07:45 +0200

    parsing bplist (v2)

--- bplist_parser.dart
+++ bplist_parser.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:path/path.dart';
+
 // part 'acb.mapper.dart';
 
 // @MappableClass()
@@ -14,6 +16,11 @@ class SimplePalette /*with SimplePaletteMappable*/ {
     required this.name,
     required this.colors,
   });
+
+  @override
+  String toString() {
+    return 'SimplePalette{version: $version, name: $name, colors: $colors}';
+  }
 }
 
 // @MappableClass()
@@ -27,8 +34,12 @@ class SimpleColor /*with SimpleColorMappable*/ {
     required this.values,
     required this.colorSpace,
   });
-}
 
+  @override
+  String toString() {
+    return 'SimpleColor{name: $name, values: $values, colorSpace: $colorSpace}';
+  }
+}
 
 const _colorSpaceRgb = 1;
 
@@ -41,76 +52,99 @@ void main(List<String> arguments) {
   final filePath = arguments[0];
 
   final data = File(filePath).readAsBytesSync();
-  final result = parseBinaryBytes(data);
+  final result = parseBinaryPlist(data);
 
   final map = Map<String, dynamic>.from(result as Map<dynamic, dynamic>);
 
-  final colors = extractColors(map);
-
-  final parsedColors = colors.map((key, value) {
-    final rgbValues = value
-        .split(' ')
-        .map((s) => double.tryParse(s) ?? int.tryParse(s)?.toDouble() ?? 0)
-        .toList();
-    return MapEntry(key, rgbValues);
-  });
-  print(parsedColors);
+  final palette = _extractSimplePalette(filePath, map);
+  print(palette);
 }
 
-Map<String, String> extractColors(Map<String, dynamic> map) {
-  if (!map.containsKey(r'$objects')) {
-    return {};
+SimplePalette _extractSimplePalette(
+  String filePath,
+  Map<String, dynamic> map,
+) {
+  final version = _extractVersion(map);
+  final paletteName = basenameWithoutExtension(filePath);
+  final objects = _extractObjects(map);
+  final extractedStrings = _extractStrings(objects);
+  final extractedMaps = _extractMaps(objects);
+  final colors = _extractColors(extractedMaps);
+
+  final colorNames = extractedStrings;
+  final colorValues = colors;
+  if (colorNames.length != colorValues.length) {
+    throw Exception('Mismatch between color names and color values');
   }
 
+  final simpleColors = List<SimpleColor>.generate(
+    colorNames.length,
+    (index) => SimpleColor(
+      name: colorNames[index],
+      values: colorValues[index],
+      colorSpace: _colorSpaceRgb,
+    ),
+  );
+
+  return SimplePalette(
+    version: version,
+    name: paletteName,
+    colors: simpleColors,
+  );
+}
+
+int _extractVersion(Map<String, dynamic> map) {
+  if (!map.containsKey(r'$version')) {
+    throw Exception('Unable to extract version from the plist');
+  }
+  return map[r'$version'] as int;
+}
+
+List<dynamic> _extractObjects(Map<String, dynamic> map) {
+  if (!map.containsKey(r'$objects')) {
+    throw Exception('Unable to extract objects from the plist');
+  }
   final objects = map[r'$objects'];
   if (objects is! List<dynamic>) {
-    return {};
+    throw Exception('Objects in the plist are not in the expected format');
   }
+  return objects;
+}
 
-  final extractedStrings = objects
+List<String> _extractStrings(List<dynamic> objects) {
+  return objects
       .whereType<String>()
       .where((str) => str != r'$null' && str.isNotEmpty)
       .toList();
+}
 
-  final extractedMaps = objects
+List<Map<String, dynamic>> _extractMaps(List<dynamic> objects) {
+  return objects
       .whereType<Map<dynamic, dynamic>>()
       .map(Map<String, dynamic>.from)
       .toList();
-
-  final nsrgbValues = extractedMaps //
-      .where((map) => map.containsKey('NSRGB'))
-      .map((map) {
-    final nsrgbValue = map['NSRGB'];
-    return nsrgbValue is List<int>
-        ? String.fromCharCodes(nsrgbValue)
-        : nsrgbValue.toString();
-  }).toList();
-
-  final nsComponentsValues = extractedMaps //
-      .where((map) => map.containsKey('NSComponents'))
-      .map((map) {
-    final nsComponentsValue = map['NSComponents'];
-    return nsComponentsValue is List<int>
-        ? String.fromCharCodes(nsComponentsValue)
-        : nsComponentsValue.toString();
-  }).toList();
-  print(nsComponentsValues);
-
-  // Create a map matching color names to NSRGB values
-  final colorMap = <String, String>{};
-  if (extractedStrings.length == nsrgbValues.length) {
-    for (var i = 0; i < extractedStrings.length; i++) {
-      colorMap[extractedStrings[i]] = nsrgbValues[i];
-    }
-  } else {
-    print('''
-Warning: The number of extracted strings does not match the number of NSRGB values.''');
-  }
-
-  return colorMap;
 }
 
-dynamic parseBinaryBytes(Uint8List dataBytes) {
+List<List<double>> _extractColors(List<Map<String, dynamic>> extractedMaps) {
+  double parseDouble(String string) {
+    return double.tryParse(string) ?? int.tryParse(string)?.toDouble() ?? 0;
+  }
+
+  final colors = <List<double>>[];
+  for (final map in extractedMaps) {
+    if (map.containsKey('NSComponents')) {
+      final nsComponentsChars = map['NSComponents'];
+      if (nsComponentsChars is List<int>) {
+        final nsComponents = String.fromCharCodes(nsComponentsChars);
+        final values = nsComponents.split(' ').map(parseDouble).toList();
+        colors.add(values);
+      }
+    }
+  }
+  return colors;
+}
+
+dynamic parseBinaryPlist(Uint8List dataBytes) {
   // Only bplist00 format is supported
   if (String.fromCharCodes(dataBytes.getRange(0, 8)) != 'bplist00') {
     throw UnimplementedError('Invalid binary plist format');
@@ -134,132 +168,176 @@ dynamic parseBinaryBytes(Uint8List dataBytes) {
   );
 
   // Get the starting position of the object
-  final objectStartPos = _getObjectStartPos(binaryData, startPos);
+  final objectStartPos = _getObjectStartPosition(binaryData, startPos);
 
   // Handle the binary data starting from the object's position
-  return _handleBinary(binaryData, objectStartPos);
+  return _parseBinaryObject(binaryData, objectStartPos);
 }
 
-dynamic _handleBinary(_BinaryData binary, int pos) {
+dynamic _parseBinaryObject(_BinaryData binary, int pos) {
   final byte = binary.bytes[pos];
-  switch (byte & 0xF0) {
-    // data
-    case 0x40:
-      final byteData = _getObjectDataBytes(
-        binary.bytes,
-        byte,
-        pos,
-      );
-      return Uint8List.view(
-        binary.bytes.buffer,
-        byteData.offsetInBytes,
-        byteData.lengthInBytes,
-      );
-
-    // string
-    case 0x50:
-      final byteData = _getObjectDataBytes(
-        binary.bytes,
-        byte,
-        pos,
-      );
-      final charCodes = Uint8List.view(
-        byteData.buffer,
-        byteData.offsetInBytes,
-        byteData.lengthInBytes,
-      );
-      return String.fromCharCodes(charCodes);
-
-    // unicode
-    case 0x60:
-      final byteData = _getObjectDataBytes(
-        binary.bytes,
-        byte,
-        pos,
-        unitByte: 2,
-      );
-      final charCodes = Uint16List.view(
-        byteData.buffer,
-        byteData.offsetInBytes,
-        byteData.lengthInBytes ~/ 2,
-      );
-      return String.fromCharCodes(charCodes);
-
-    // array
-    case 0xA0:
-      // Get the byte data for the array
-      final byteData = _getObjectDataBytes(
-        binary.bytes,
-        byte,
-        pos,
-        unitByte: binary.objectRefSize,
-      );
-
-      // Calculate the number of items in the array
-      final itemCount = byteData.lengthInBytes ~/ binary.objectRefSize;
-
-      // Generate a list of parsed objects
-      return List.generate(itemCount, (index) {
-        // Calculate the position of the current item
-        final itemOffset = index * binary.objectRefSize;
-        final itemRef =
-            _bytesToInt(byteData, binary.objectRefSize, offset: itemOffset);
-        final itemPos = _getObjectStartPos(binary, itemRef);
-
-        // Parse and return the item
-        return _handleBinary(binary, itemPos);
-      });
-
-    // dictionary
-    case 0xD0:
-      // Get the byte data for the dictionary
-      final byteData = _getObjectDataBytes(
-        binary.bytes,
-        byte,
-        pos,
-        unitByte: binary.objectRefSize,
-        sizeScale: 2,
-      );
-
-      // Calculate the number of key-value pairs
-      final pairCount = byteData.lengthInBytes ~/ (2 * binary.objectRefSize);
-
-      // Create a map to store the key-value pairs
-      final map = <dynamic, dynamic>{};
-
-      // Iterate through each key-value pair
-      for (var i = 0; i < pairCount; i++) {
-        // Calculate offsets for key and value
-        final keyOffset = i * binary.objectRefSize;
-        final valueOffset = (i + pairCount) * binary.objectRefSize;
-
-        // Get positions for key and value
-        final keyPos = _getObjectStartPos(
-          binary,
-          _bytesToInt(byteData, binary.objectRefSize, offset: keyOffset),
-        );
-        final valuePos = _getObjectStartPos(
-          binary,
-          _bytesToInt(byteData, binary.objectRefSize, offset: valueOffset),
-        );
-
-        // Parse key and value
-        final key = _handleBinary(binary, keyPos);
-        final value = _handleBinary(binary, valuePos);
-
-        // Add key-value pair to the map
-        map[key] = value;
-      }
-
-      return map;
-  }
-  return null;
+  return switch (byte & 0xF0) {
+    0x00 => _parseBoolObject(byte),
+    0x10 => _parseIntegerObject(binary, byte, pos),
+    0x20 => _parseRealObject(binary, byte, pos),
+    0x30 => _parseDateObject(binary, pos),
+    0x40 => _parseDataObject(binary, byte, pos),
+    0x50 => _parseStringObject(binary, byte, pos),
+    0x60 => _parseUnicodeObject(binary, byte, pos),
+    0xA0 => _parseArrayObject(binary, byte, pos),
+    0xD0 => _parseDictionaryObject(binary, byte, pos),
+    _ => null,
+  };
 }
 
-int _getObjectStartPos(_BinaryData binary, int offset) {
+bool? _parseBoolObject(int byte) {
+  return switch (byte) {
+    0x08 => false,
+    0x09 => true,
+    _ => null,
+  };
+}
+
+int _parseIntegerObject(_BinaryData binary, int byte, int pos) {
+  final length = 1 << (byte & 0xf);
+  return _convertBytesToInt(
+    binary.bytes.buffer.asByteData(pos + 1, length),
+    length,
+    signed64Bit: true,
+  );
+}
+
+double _parseRealObject(_BinaryData binary, int byte, int pos) {
+  final length = 1 << (byte & 0xf);
+  return _convertBytesToDouble(
+    binary.bytes.buffer.asByteData(pos + 1, length),
+    length,
+  );
+}
+
+DateTime _parseDateObject(_BinaryData binary, int pos) {
+  // Extract 8 bytes of data starting from pos + 1
+  final data = binary.bytes.buffer.asByteData(pos + 1, 8);
+  // Convert the bytes to a double representing seconds
+  final secondsSince2001 = _convertBytesToDouble(data, 8);
+  // Convert to microseconds and adjust for Unix epoch (1970-01-01)
+  final microsecondsSinceEpoch =
+      ((secondsSince2001 + 978307200) * 1000000).toInt();
+  // Create and return a UTC DateTime object
+  return DateTime.fromMicrosecondsSinceEpoch(
+    microsecondsSinceEpoch,
+    isUtc: true,
+  );
+}
+
+Uint8List _parseDataObject(_BinaryData binary, int byte, int pos) {
+  final byteData = _getObjectDataBytes(
+    binary.bytes,
+    byte,
+    pos,
+  );
+  return Uint8List.view(
+    binary.bytes.buffer,
+    byteData.offsetInBytes,
+    byteData.lengthInBytes,
+  );
+}
+
+String _parseStringObject(_BinaryData binary, int byte, int pos) {
+  final byteData = _getObjectDataBytes(
+    binary.bytes,
+    byte,
+    pos,
+  );
+  final charCodes = Uint8List.view(
+    byteData.buffer,
+    byteData.offsetInBytes,
+    byteData.lengthInBytes,
+  );
+  return String.fromCharCodes(charCodes);
+}
+
+String _parseUnicodeObject(_BinaryData binary, int byte, int pos) {
+  final byteData = _getObjectDataBytes(
+    binary.bytes,
+    byte,
+    pos,
+    unitByte: 2,
+  );
+  final charCodes = Uint16List.view(
+    byteData.buffer,
+    byteData.offsetInBytes,
+    byteData.lengthInBytes ~/ 2,
+  );
+  return String.fromCharCodes(charCodes);
+}
+
+List<dynamic> _parseArrayObject(_BinaryData binary, int byte, int pos) {
+  // Get the byte data for the array
+  final byteData = _getObjectDataBytes(
+    binary.bytes,
+    byte,
+    pos,
+    unitByte: binary.objectRefSize,
+  );
+  // Calculate the number of items in the array
+  final itemCount = byteData.lengthInBytes ~/ binary.objectRefSize;
+  // Generate a list of parsed objects
+  return List.generate(itemCount, (index) {
+    // Calculate the position of the current item
+    final itemOffset = index * binary.objectRefSize;
+    final itemRef =
+        _convertBytesToInt(byteData, binary.objectRefSize, offset: itemOffset);
+    final itemPos = _getObjectStartPosition(binary, itemRef);
+    // Parse and return the item
+    return _parseBinaryObject(binary, itemPos);
+  });
+}
+
+Map<dynamic, dynamic> _parseDictionaryObject(
+  _BinaryData binary,
+  int byte,
+  int pos,
+) {
+  // Get the byte data for the dictionary
+  final byteData = _getObjectDataBytes(
+    binary.bytes,
+    byte,
+    pos,
+    unitByte: binary.objectRefSize,
+    sizeScale: 2,
+  );
+  // Calculate the number of key-value pairs
+  final pairCount = byteData.lengthInBytes ~/ (2 * binary.objectRefSize);
+  // Create a map to store the key-value pairs
+  final map = <dynamic, dynamic>{};
+  // Iterate through each key-value pair
+  for (var i = 0; i < pairCount; i++) {
+    // Calculate offsets for key and value
+    final keyOffset = i * binary.objectRefSize;
+    final valueOffset = (i + pairCount) * binary.objectRefSize;
+    // Get positions for key and value
+    final keyPos = _getObjectStartPosition(
+      binary,
+      _convertBytesToInt(byteData, binary.objectRefSize, offset: keyOffset),
+    );
+    final valuePos = _getObjectStartPosition(
+      binary,
+      _convertBytesToInt(byteData, binary.objectRefSize, offset: valueOffset),
+    );
+    // Parse key and value
+    final key = _parseBinaryObject(binary, keyPos);
+    final value = _parseBinaryObject(binary, valuePos);
+    // Add key-value pair to the map
+    map[key] = value;
+  }
+  return map;
+}
+
+int _getObjectStartPosition(_BinaryData binary, int offset) {
   final keyRefPos =
       (binary.offsetTableStartPos) + (binary.offsetTableOffsetSize * offset);
-  return _bytesToInt(
+  return _convertBytesToInt(
     binary.bytes.buffer.asByteData(keyRefPos, binary.offsetTableOffsetSize),
     binary.offsetTableOffsetSize,
   );
@@ -282,7 +360,7 @@ ByteData _getObjectDataBytes(
     final size = 1 << exponentBits; // Equivalent to 2^exponentBits
 
     localPos++;
-    length = _bytesToInt(bytes.buffer.asByteData(localPos, size), size) *
+    length = _convertBytesToInt(bytes.buffer.asByteData(localPos, size), size) *
         unitByte *
         sizeScale;
 
@@ -296,20 +374,28 @@ ByteData _getObjectDataBytes(
   return bytes.buffer.asByteData(localPos, length);
 }
 
-int _bytesToInt(
+int _convertBytesToInt(
   ByteData byteData,
   int byteSize, {
   int offset = 0,
   bool signed64Bit = false,
 }) {
-  if (byteData.lengthInBytes < byteSize) {
-    throw Exception('bytes list size is invalid');
-  }
   return switch (byteSize) {
     1 => byteData.getUint8(offset),
     2 => byteData.getUint16(offset),
     4 => byteData.getUint32(offset),
     8 => signed64Bit ? byteData.getInt64(offset) : byteData.getUint64(offset),
+    _ => throw Exception('Undefined ByteSize: $byteSize'),
+  };
+}
+
+double _convertBytesToDouble(
+  ByteData byteData,
+  int byteSize,
+) {
+  return switch (byteSize) {
+    4 => byteData.getFloat32(0),
+    8 => byteData.getFloat64(0),
     _ => throw Exception('Undefined ByteSize: $byteSize'),
   };
 }
@@ -329,6 +415,7 @@ class _BinaryData {
 
   @override
   String toString() {
-    return '_BinaryData{bytes.length: ${bytes.length}, offsetTableStartPos: $offsetTableStartPos, offsetTableOffsetSize: $offsetTableOffsetSize, objectRefSize: $objectRefSize}';
+    return '''
+_BinaryData{bytes.length: ${bytes.length}, offsetTableStartPos: $offsetTableStartPos, offsetTableOffsetSize: $offsetTableOffsetSize, objectRefSize: $objectRefSize}''';
   }
 }
